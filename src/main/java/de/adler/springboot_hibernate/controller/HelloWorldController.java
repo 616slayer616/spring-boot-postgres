56e60fa77bd40137a41d19d7f91fac2e556763c6
adler <patrick.adler@adesso.de>
2017-03-11 17:23:13 +0100

    Revert "Refactor"
    This reverts commit 1135fb03091baa5bad7e3e0b03cf24fd5db22376.

---
+++ src/main/java/de/adler/springboot_hibernate/controller/HelloWorldController.java
@@ -0,0 +1,43 @@
+package de.adler.springboot_hibernate.controller;
+
+
+import de.adler.springboot_hibernate.database.entity.Customer;
+import de.adler.springboot_hibernate.database.repository.CustomerRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequestMapping("/")
+@CrossOrigin(/*origins = "http://localhost:4200"*/)
+class HelloWorldController {
+
+    private static final Logger log = Logger.getLogger(HelloWorldController.class);
+
+    @SuppressWarnings("CanBeFinal")
+    @Autowired
+    private CustomerRepository customers;
+
+    @ResponseBody
+    @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok().cacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES)).body("Hallol");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "customer/{lastName}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<List<Customer>> customer(@PathVariable String lastName) {
+        List<Customer> result = customers.findByLastName(lastName);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.SECONDS))
+                .body(result);
+    }
+
+}
---
+++ src/main/java/de/adler/springboot_hibernate/database/repository/CustomerRepository.java
@@ -0,0 +1,20 @@
+package de.adler.springboot_hibernate.database.repository;
+
+import de.adler.springboot_hibernate.database.entity.Customer;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * @author adler
+ * @version 01.03.2017
+ */
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    @Query("SELECT p FROM Customer p WHERE LOWER(p.lastName) = LOWER(:lastName)")
+    List<Customer> findByLastNameCaseInsensitive(@Param("lastName") String lastName);
+}
