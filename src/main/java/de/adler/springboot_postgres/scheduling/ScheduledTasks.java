83ab8de7f8f3b7fba62c63593abe4850497694dc
adler <patrick.adler@adesso.de>
2017-03-15 15:11:31 +0100

    Spring scheduling

---
+++ src/main/java/de/adler/springboot_postgres/scheduling/ScheduledTasks.java
@@ -0,0 +1,37 @@
+package de.adler.springboot_postgres.scheduling;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Value("${spring.scheduling.cron}")
+    private String cronValue;
+
+    @Bean
+    public String cronBean() {
+        return this.cronValue;
+    }
+
+    @Scheduled(initialDelay = 5000, fixedRateString = "${spring.scheduling.fixed}")
+    public void scheduleTaskUsingfidexRate() {
+        log.info("Running scheduleTaskUsingfidexRate: The time is now {}", dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "#{@cronBean}")
+    public void scheduleTaskUsingCronExpression() {
+        log.info("Running scheduleTaskUsingCronExpression: The time is now {}", dateFormat.format(new Date()));
+    }
+}
