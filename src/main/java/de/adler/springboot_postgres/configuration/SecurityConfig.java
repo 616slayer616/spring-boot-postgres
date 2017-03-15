ac4f69fef2cdcd0da8409158d043e49e3576c716
Patrick Adler <patrick.adler92@gmail.com>
2017-03-15 21:15:42 +0100

    Added role hierarchy

--- src/main/java/de/adler/springboot_postgres/configuration/SecurityConfig.java
+++ src/main/java/de/adler/springboot_postgres/configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package de.adler.springboot_postgres.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,5 +51,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery(usersQuery)
                 .authoritiesByUsernameQuery(rolesQuery)
                 .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER > ROLE_GUEST");
+        return roleHierarchy;
     }
 }
